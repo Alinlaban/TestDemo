2d954012fc189f4329fa80e8e29e3619779f9609
Alinlaban <zfldiv@163.com>
2017-08-22 16:39:49 +0800

    commit

---
+++ src/com/test/servlet/UserServlet.java
@@ -0,0 +1,54 @@
+package com.test.servlet;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/user")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie[] cookies = request.getCookies();
+		if (cookies!=null) {
+			for (int i = 0; i < cookies.length; i++) {
+				System.out.println(cookies[i].getValue());
+			}
+		}
+		//cookies[0].s
+		
+		//if ("zhao".equals(request.getSession().getAttribute("user"))) {
+			request.getRequestDispatcher("user.jsp").forward(request, response);
+		//} else {
+		//	request.getSession().setAttribute("user", "zhao");
+		//}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
